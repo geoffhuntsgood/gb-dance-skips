e8dbea23d5091c9ae6905a73edc469a33a11bcae
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-09-06 13:58:02 -0400

    Don't skip dances in races where returning to the parent map breaks (or where it looks weird)

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 extern Actor *gCurrentPlayer;
 extern u8 cc_player_index;
+extern Maps current_map;
 
 void func_global_asm_80602B60(s32 arg0, u8 arg1);
 void func_global_asm_806CA2AC(void);
@@ -15,12 +16,33 @@ s16 func_global_asm_80672A70(s16 arg0, s16 arg1);
 s16 func_global_asm_80665DE0(f32 arg0, f32 arg1, f32 arg2, f32 arg3);
 void func_global_asm_80627948(Actor *arg0, u8 arg1, u8 arg2, u8 arg3);
 
-// danceGB
+void vanilla_dance_gb(void) {
+  func_global_asm_80602B60(0x6B, 0);
+  func_global_asm_806CA2AC();
+  func_global_asm_806C9F74(gCurrentPlayer);
+  func_global_asm_806CF42C(gCurrentPlayer);
+  playAnimation(gCurrentPlayer, 0x5B);
+  gCurrentPlayer->control_state = 0x70;
+  gCurrentPlayer->control_state_progress = 0;
+  
+  if (gCurrentPlayer->unkFC) {
+    gCurrentPlayer->y_rotation = func_global_asm_80672A70(gCurrentPlayer->unkF4, gCurrentPlayer->unkF6);
+  } else {
+    gCurrentPlayer->y_rotation = func_global_asm_80665DE0(
+      character_change_array[cc_player_index].look_at_eye_x,
+      character_change_array[cc_player_index].look_at_eye_z,
+      gCurrentPlayer->x_position,
+      gCurrentPlayer->z_position
+    );
+  }
+  func_global_asm_80627948(gCurrentPlayer, 0x15, 0x1F, 5);
+}
+
 /*
   Keeping comments to show what's changed
   from the vanilla func for debugging purposes.
 */
-RECOMP_PATCH void func_global_asm_806EFB4C(void) {
+void modified_dance_gb(void) {
   func_global_asm_80602B60(0x6B, 0); // cancelMusic
   func_global_asm_806CA2AC(); // ResetShockwaveTimer
   func_global_asm_806C9F74(gCurrentPlayer); // FUN_806c9f74 (??)
@@ -41,4 +63,22 @@ RECOMP_PATCH void func_global_asm_806EFB4C(void) {
     }
     func_global_asm_80627948(gCurrentPlayer, 0x15, 0x1F, 5); // FUN_80627948 (??)
   */
+}
+
+// danceGB patch
+RECOMP_PATCH void func_global_asm_806EFB4C(void) {
+  if (
+    current_map == MAP_AZTEC_BEETLE_RACE ||
+    current_map == MAP_FACTORY_CAR_RACE ||
+    current_map == MAP_GALLEON_SEAL_RACE ||
+    current_map == MAP_CAVES_BEETLE_RACE ||
+    current_map == MAP_CASTLE_CAR_RACE ||
+    current_map == MAP_JAPES_MINECART ||
+    current_map == MAP_FUNGI_MINECART ||
+    current_map == MAP_CASTLE_MINECART
+  ) {
+    vanilla_dance_gb();
+  } else {
+    modified_dance_gb();
+  }
 }
