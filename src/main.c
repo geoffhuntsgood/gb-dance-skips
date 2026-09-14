aaabd4986f806f9ffe4420d2a2e7396f051ff1ed
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-09-13 21:09:32 -0400

    code cleanup/remove debug comments

--- src/main.c
+++ src/main.c
@@ -3,19 +3,21 @@
 #include "enums.h"
 #include "common_structs.h"
 
-extern Actor *gCurrentPlayer;
 extern u8 cc_player_index;
+extern Actor *gCurrentPlayer;
 extern Maps current_map;
 
 void func_global_asm_80602B60(s32 arg0, u8 arg1);
-void func_global_asm_806CA2AC(void);
+void func_global_asm_80627948(Actor *arg0, u8 arg1, u8 arg2, u8 arg3);
 void func_global_asm_806C9F74(Actor *arg0);
+void func_global_asm_806CA2AC(void);
 void func_global_asm_806CF42C(Actor *arg0);
 void playAnimation(Actor *arg0, s32 arg2);
-s16 func_global_asm_80672A70(s16 arg0, s16 arg1);
-s16 func_global_asm_80665DE0(f32 arg0, f32 arg1, f32 arg2, f32 arg3);
-void func_global_asm_80627948(Actor *arg0, u8 arg1, u8 arg2, u8 arg3);
 
+s16 func_global_asm_80665DE0(f32 arg0, f32 arg1, f32 arg2, f32 arg3);
+s16 func_global_asm_80672A70(s16 arg0, s16 arg1);
+
+// Copy of func_global_asm_806EFB4C
 void vanilla_dance_gb(void) {
   func_global_asm_80602B60(0x6B, 0);
   func_global_asm_806CA2AC();
@@ -38,31 +40,14 @@ void vanilla_dance_gb(void) {
   func_global_asm_80627948(gCurrentPlayer, 0x15, 0x1F, 5);
 }
 
-/*
-  Keeping comments to show what's changed
-  from the vanilla func for debugging purposes.
-*/
 void modified_dance_gb(void) {
-  func_global_asm_80602B60(0x6B, 0); // cancelMusic
-  func_global_asm_806CA2AC(); // ResetShockwaveTimer
-  func_global_asm_806C9F74(gCurrentPlayer); // FUN_806c9f74 (??)
-  func_global_asm_806CF42C(gCurrentPlayer); // DestroyHeldObject
+  func_global_asm_80602B60(0x6B, 0);
+  func_global_asm_806CA2AC();
+  func_global_asm_806C9F74(gCurrentPlayer);
+  func_global_asm_806CF42C(gCurrentPlayer);
   playAnimation(gCurrentPlayer, 0x5B);
-  gCurrentPlayer->control_state = 0; // Changed from 0x70
+  gCurrentPlayer->control_state = 0;
   gCurrentPlayer->control_state_progress = 0;
-  /* if/else reversed in decomp
-    if (gCurrentPlayer->unkFC) {
-      gCurrentPlayer->y_rotation = func_global_asm_80672A70(gCurrentPlayer->unkF4, gCurrentPlayer->unkF6); // FUN_80672a70 (??)
-    } else {
-      gCurrentPlayer->y_rotation = func_global_asm_80665DE0(
-        character_change_array[cc_player_index].look_at_eye_x,
-        character_change_array[cc_player_index].look_at_eye_z,
-        gCurrentPlayer->x_position,
-        gCurrentPlayer->z_position
-      ); // getAngleBetweenPoints
-    }
-    func_global_asm_80627948(gCurrentPlayer, 0x15, 0x1F, 5); // FUN_80627948 (??)
-  */
 }
 
 // danceGB patch
